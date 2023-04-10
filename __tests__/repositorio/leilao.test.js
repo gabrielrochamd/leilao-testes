294492c5d0bbd2126b2c5ff51c8f3826c5617abd
Gabriel Rocha <gabrielmrocha@outlook.com.br>
2023-04-10 15:24:03 -0300

    Cria testes para o repositório utilizando mocks para a API

---
+++ __tests__/repositorio/leilao.test.js
@@ -0,0 +1,36 @@
+import { obtemLeiloes } from '../../src/repositorio/leilao';
+import apiLeiloes from '../../src/servicos/apiLeiloes';
+
+jest.mock('../../src/servicos/apiLeiloes');
+
+const mockLeiloes = [
+  { id: 1, nome: 'Leilão', descricao: 'Descrição do leilão' }
+];
+
+function mockRequisicao(retorno) {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve({ data: retorno }), 200);
+  });
+}
+
+function mockRequisicaoErro() {
+  return new Promise((_, reject) => {
+    setTimeout(() => reject(), 200);
+  });
+}
+
+describe('repositorio/leilao', () => {
+  describe(obtemLeiloes.name, () => {
+    it('deve retornar uma lista de leilões', async () => {
+      apiLeiloes.get.mockImplementation(() => mockRequisicao(mockLeiloes));
+      const leiloes = await obtemLeiloes();
+      expect(leiloes).toEqual(mockLeiloes);
+    });
+
+    it('deve retornar uma lista vazia quando a requisição falhar', async () => {
+      apiLeiloes.get.mockImplementation(() => mockRequisicaoErro());
+      const leiloes = await obtemLeiloes();
+      expect(leiloes).toEqual([]);
+    });
+  });
+});
